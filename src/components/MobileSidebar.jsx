da2a9a710cbdaf7a48296d316c88f929f8daeac1
Armand2312 <armand.oral231@gmail.com>
2024-03-22 14:54:26 +0000

    account/sidebar

--- src/components/MobileSidebar.jsx
+++ src/components/MobileSidebar.jsx
@@ -2,7 +2,7 @@
 import { useContext } from "react"
 import { CartContext } from "./CartContext";
 import Link from "next/link";
-import { CartIcon, HomeIcon, ProductsIcon } from "../../public/Icons";
+import { AccountIcon, CartIcon, HomeIcon, ProductsIcon } from "../../public/Icons";
 import { usePathname } from "next/navigation";
 
 export default function MobileSideBar({ show }) {
@@ -10,26 +10,29 @@ export default function MobileSideBar({ show }) {
     const pathname = usePathname();
 
     return (
-        <aside className={`${show ? "left-0" : "left-full"} md:hidden fixed w-full text-white transition-all p-6 bg-gray-800 h-full z-10`}>
+        <aside className={`${show ? "left-0" : "left-full"} flex flex-col justify-between md:hidden fixed w-full transition-all p-6 bg-gray-800 h-screen z-10`}>
             <div className="flex flex-col space-y-4">
-                
                 <Link className={`text-lg flex items-center gap-1 p-2 rounded-3xl pl-5 ${pathname === "/" ? "text-white bg-blue-500" : "text-gray-400"}`} href={"/"} scroll={false}>
                     <HomeIcon sizes={20} color={`${pathname === "/" ? "white" : "#9ca3af"}`} />
                     <h2>Home</h2>
                 </Link>
-                
+
                 <Link className={`text-lg flex items-center gap-1 p-2 rounded-3xl pl-5 ${pathname.includes("products") ? "text-white bg-blue-500" : "text-gray-400"}`} href={"/products"} scroll={false}>
                     <ProductsIcon sizes={20} color={`${pathname.includes("products") ? "white" : "#9ca3af"}`} />
                     <h2>Products</h2>
                 </Link>
-                
+
                 <Link className={`text-lg flex items-center gap-1 p-2 rounded-3xl pl-5 ${pathname.includes("cart") ? "text-white bg-blue-500" : "text-gray-400"}`} href={"/cart"} scroll={false}>
                     <CartIcon sizes={20} color={`${pathname.includes("cart") ? "white" : "#9ca3af"}`} />
                     <h2>Cart</h2>
                     ({cartProducts.length})
                 </Link>
-                
-
+            </div>
+            <div className="mb-[100px]">
+                <Link className={`text-lg flex items-center gap-1 p-2 rounded-3xl pl-5 ${pathname.includes("account") ? "text-white bg-blue-500" : "text-gray-400"}`} href={"/account"} scroll={false}>
+                    <AccountIcon sizes={20} color={`${pathname.includes("account") ? "white" : "#9ca3af"}`} />
+                    <h2>Account</h2>
+                </Link>
             </div>
         </aside>
     )
